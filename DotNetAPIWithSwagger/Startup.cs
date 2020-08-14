8940f78962b41f11af00706d6e32ea1cca81d61a
Ömer İPEK <omer.ipk@gmail.com>
2020-08-14 10:01:21 +0300

    Add project files.

---
+++ DotNetAPIWithSwagger/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetAPIWithSwagger
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // Swagger Info: Middleware swagger bind ediliyor.
+            services.AddSwaggerGen();
+
+            // Swagger Info: Customize edilen swagger option'ları bind ediliyor.
+            services.AddSwagger();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Swagger Info: Swagger bind ediliyor istenirse configurede edilebilir.
+            app.UseSwagger();
+
+            // Swagger Info: Swagger UI configure ediliyor.
+            app.UseSwaggerUI(x =>
+            {
+                // Swagger Info:  Swagger UI'da api'lerimizi görüntülemesi için json yolu ve swagger name set ediliyor.
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "My .Net Core API");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ DotNetAPIWithSwagger/StartupExtenstions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace DotNetAPIWithSwagger
+{
+    public static class StartupExtenstions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Swagger Example API",
+                    Version = "v1",
+                    Description = "This is Example for Github"
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Please write a token into the field. Example: \"Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme 
+                        {
+                            Reference = new OpenApiReference 
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        }, Array.Empty<string>()
+                    } 
+                });
+            });
+
+            return services;
+        }
+    }
+}
